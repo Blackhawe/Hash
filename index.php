518cdd7291b0b706d360b81de4c9886a63633473
Gilberto Yosemir González Méndez <gilbertogonzalez3a@gmail.com>
2023-05-28 00:16:22 -0600

    Agregue index.php y estilos CSS

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cifrado SHA1</title>
+
+    <link rel="stylesheet" href="style-SHA1.css" />
+</head>
+<body>
+    <form class="form" method="POST">
+        <h3>Cifrado hash SHA1</h3>
+        <p type="Cifrar texto:"><input placeholder="Escriba aquí.." type="text" name="cifrar" required></input></p>
+        <p type="Texto cifrado:"></p>
+        <p id="cifrado">
+            <?php
+                if (isset($_POST['cifrar'])) {
+                    $cifra = $_POST['cifrar'];
+                    $prueba = hash("sha1", $cifra);
+                    echo $prueba;
+                }
+            ?>
+        </p>
+        <button>CIFRAR</button>
+    </form>
+</body>
+</html>
